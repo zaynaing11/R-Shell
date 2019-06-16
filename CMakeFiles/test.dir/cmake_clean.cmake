53f520bae7f904a0a2dfe21f75397af01b826235
zaynaing11 <zaynaing11@gmail.com>
2019-06-15 19:36:35 -0700

    Create cmake_clean.cmake

---
+++ CMakeFiles/test.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test.dir/unit_tests/test.cpp.o"
+  "CMakeFiles/test.dir/src/commands.cpp.o"
+  "CMakeFiles/test.dir/src/connectors.cpp.o"
+  "CMakeFiles/test.dir/src/prototype.cpp.o"
+  "test.pdb"
+  "test"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
