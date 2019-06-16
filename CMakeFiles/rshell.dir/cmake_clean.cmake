f134dce441d0ac93a45f863ddacfa6f7c3702599
zaynaing11 <zaynaing11@gmail.com>
2019-06-15 19:36:11 -0700

    Create cmake_clean.cmake

---
+++ CMakeFiles/rshell.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rshell.dir/src/rshell.cpp.o"
+  "CMakeFiles/rshell.dir/src/commands.cpp.o"
+  "CMakeFiles/rshell.dir/src/connectors.cpp.o"
+  "rshell.pdb"
+  "rshell"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rshell.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
