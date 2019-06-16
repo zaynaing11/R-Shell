2254ab29c0ffd567ede062e8e87fe430bc7b9336
zaynaing11 <zaynaing11@gmail.com>
2019-06-15 19:36:43 -0700

    Create DependInfo.cmake

---
+++ CMakeFiles/test.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/csmajs/ksiva001/assignment-zaddy/src/commands.cpp" "/home/csmajs/ksiva001/assignment-zaddy/CMakeFiles/test.dir/src/commands.cpp.o"
+  "/home/csmajs/ksiva001/assignment-zaddy/src/connectors.cpp" "/home/csmajs/ksiva001/assignment-zaddy/CMakeFiles/test.dir/src/connectors.cpp.o"
+  "/home/csmajs/ksiva001/assignment-zaddy/src/prototype.cpp" "/home/csmajs/ksiva001/assignment-zaddy/CMakeFiles/test.dir/src/prototype.cpp.o"
+  "/home/csmajs/ksiva001/assignment-zaddy/unit_tests/test.cpp" "/home/csmajs/ksiva001/assignment-zaddy/CMakeFiles/test.dir/unit_tests/test.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "gtest_disable_pthreads=ON"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "googletest/googletest/include"
+  "googletest/googletest"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/csmajs/ksiva001/assignment-zaddy/googletest/googlemock/gtest/CMakeFiles/gtest.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
